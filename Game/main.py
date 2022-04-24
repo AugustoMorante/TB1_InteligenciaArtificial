81ddfe57ddb44fdafcce98cc12713d54734056d9
Augusto Morante <augustomorante@hotmail.com>
2022-04-23 20:56:54 -0500

    game implementation

--- Game/main.py
+++ Game/main.py
@@ -1,15 +1,18 @@
 from email.mime import image
 from math import fabs
+from operator import ge
 from pickle import TRUE
 from turtle import speed, width
 import pygame, sys, random
 import pygame.locals as GAME_GLOBALS
 import pygame.event as GAME_EVENTS
 import pygame.time as GAME_TIME
+import numpy as np
+import random
 
 pygame.init()
 clock = pygame.time.Clock()
-soundtrack = pygame.mixer.Sound("Game/recursos/soundtrack3.wav")
+soundtrack = pygame.mixer.Sound("Game/recursos/soundtrack.wav")
 player_image = pygame.image.load("Game/recursos/dinosaurio.png")
 title_image = pygame.image.load("Game/recursos/titulo.jpg")
 game_over_image = pygame.image.load("Game/recursos/Fin_juego.jpg")
@@ -19,7 +22,7 @@ windowHeight = 600
 
 surface = pygame.display.set_mode((windowWidth, windowHeight)) # ventana principal para nuestro juego
 
-pygame.display.set_caption('¡Déjate caer!')
+pygame.display.set_caption('DINO GAME')
 
 # Definimos las variables para las pulsaciones de las teclas
 leftDown = False
@@ -45,6 +48,87 @@ player = {
   "vy" : 5,
   "direction": "left"
 }
+
+#Algoritmo Genético
+class DNA:
+    def __init__(self, target, mutation_rate, n_individuals, n_selection, n_generations, verbose = True):
+        self.target = target
+        self.mutation_rate = mutation_rate
+        self.n_individuals = n_individuals
+        self.n_selection = n_selection
+        self.n_generations = n_generations
+        self.verbose = verbose
+
+
+    def create_individual(self, min = 0, max = 9):
+        return [np.random.randint(min, max) for _ in range(len(self.target))]
+
+    def create_population(self):
+        return [self.create_individual() for _ in range(self.n_individuals)]
+
+    def fitness(self, individual):
+        fitness = 0
+
+        for i in range(len(individual)):
+            if individual[i] == self.target[i]:
+                fitness += 1
+        
+        return fitness
+    
+    def selection(self, population):
+
+        scores = [(self.fitness(i), i) for i in population]
+        scores = [i[1] for i in sorted(scores)]
+
+        return scores[len(scores)-self.n_selection:]
+    
+    def reproduction(self, population, selected):
+
+        
+        point = 0
+        father = []
+
+        for i in range(len(population)):
+            point = np.random.randint(1, len(self.target) - 1)
+            father = random.sample(selected, 2)
+
+            population[i][:point] = father[0][:point]
+            population[i][point:] = father[1][point:]
+        
+        return population
+    
+    def mutation(self, population):
+        
+        for i in range(len(population)):
+            if random.random() <= self.mutation_rate:
+                point = np.random.randint(len(self.target))
+                new_value = np.random.randint(0, 9)
+
+                while new_value == population[i][point]:
+                    new_value = np.random.randint(0, 9)
+                
+                population[i][point] = new_value
+            return population
+    
+    def run_geneticalgo(self):
+        population = self.create_population()
+        populationList = []
+
+        for i in range(self.n_generations):
+
+            '''if self.verbose:
+                print('__')
+                print('Generacion: ', i)
+                print('Poblacion', population)
+                print()'''
+
+            temp = population[0][0]
+            populationList.append(temp)
+            selected = self.selection(population)
+            population = self.reproduction(population, selected)
+            population = self.mutation(population)
+
+        return populationList
 
 # Clase para mapear el sprite y sus propieades iniciales del dinosaurio
 class Dino(pygame.sprite.Sprite):
@@ -80,13 +164,28 @@ class Dino(pygame.sprite.Sprite):
 
 fuente = pygame.font.Font(None, 30)
 
+
+target = [9,0,0]
+model = DNA(target = target,mutation_rate = 0.2,n_individuals = 25,n_selection = 10,n_generations = 10,verbose=True)
+genetico = model.run_geneticalgo()
+
+print(" ")
+print('Con el algoritmo genético las velocidades varían:')
+print(" ")
+print('1° Generacion - Velocidad dinosaurio naranja: ', genetico[0])
+print('2° Generacion - Velocidad dinosaurio azul: ', genetico[1]) 
+print('3° Generacion - Velocidad dinosaurio celeste: ', genetico[2]) 
+print('4° Generacion - Velocidad dinosaurio rojo: ', genetico[3]) 
+print('5° Generacion - Velocidad dinosaurio amarillo: ', genetico[4]) 
+
+
 #Poblacion inicial de dinos
 dinos = []
-dino = Dino(player["x"], player["y"], random.randint(2,9), random.randint(10,70), random.randint(10,70), "Game/recursos/dinosaurio.png", (255, 127, 39))
-dino2 = Dino(player["x"]/1.1, player["y"], random.randint(2,9), random.randint(10,70), random.randint(10,70), "Game/recursos/dinosaurio2.png", (63, 72, 204))
-dino3 = Dino(player["x"]/1.2, player["y"], random.randint(2,9), random.randint(10,70), random.randint(10,70), "Game/recursos/dinosaurio3.png", (0, 168, 243))
-dino4 = Dino(player["x"]/1.3, player["y"], random.randint(2,9), random.randint(10,70), random.randint(10,70), "Game/recursos/dinosaurio4.png", (136, 0, 27))
-dino5 = Dino(player["x"]/1.4, player["y"], random.randint(2,9), random.randint(10,70), random.randint(10,70), "Game/recursos/dinosaurio5.png", (255, 202, 24))
+dino = Dino(player["x"], player["y"], genetico[0], random.randint(10,70), random.randint(10,70), "Game/recursos/dinosaurio.png", (255, 127, 39))
+dino2 = Dino(player["x"]/1.1, player["y"], genetico[1], random.randint(10,70), random.randint(10,70), "Game/recursos/dinosaurio2.png", (63, 72, 204))
+dino3 = Dino(player["x"]/1.2, player["y"], genetico[2], random.randint(10,70), random.randint(10,70), "Game/recursos/dinosaurio3.png", (0, 168, 243))
+dino4 = Dino(player["x"]/1.3, player["y"], genetico[3], random.randint(10,70), random.randint(10,70), "Game/recursos/dinosaurio4.png", (136, 0, 27))
+dino5 = Dino(player["x"]/1.4, player["y"], genetico[4], random.randint(10,70), random.randint(10,70), "Game/recursos/dinosaurio5.png", (255, 202, 24))
 nuvosDinos = []
 
 dinos.append(dino)
@@ -239,39 +338,26 @@ while True:
   for i in range(len(gamePlatforms)):
     for dino in dinos:
       if gamePlatforms[i]['pos'][1] - dino.y == dino.height + 1: 
-        print("En la plataforma: ", i + 1)
-        print("Escape: ",gamePlatforms[i]['gap'])
-        print("X: ", int(dino.x))
+        #print("En la plataforma: ", i + 1)
+        #print("Escape: ",gamePlatforms[i]['gap'])
+        #print("X: ", int(dino.x))
         dino.leftDown = False
 
         if int(dino.x) > gamePlatforms[i]['gap']:
-          print("izquierda")
+          #print("izquierda")
           dino.leftDown = True
 
         dino.rightDown = False
         if int(dino.x) < gamePlatforms[i]['gap']:
           dino.rightDown = True
-          print("derecha")
+          #print("derecha")
 
-  if len(gamePlatforms) > 0:
-    print(gamePlatforms[-1])
+  #if len(gamePlatforms) > 0:
+    #print(gamePlatforms[-1])
 
   for event in GAME_EVENTS.get():
 
     if event.type == pygame.KEYDOWN:
-
-      if event.key == pygame.K_LEFT:
-        leftDown = True
-      if event.key == pygame.K_RIGHT:
-        rightDown = True
-      if event.key == pygame.K_ESCAPE:
-        quitGame()
-
-    if event.type == pygame.KEYUP:
-      if event.key == pygame.K_LEFT:
-        leftDown = False
-      if event.key == pygame.K_RIGHT:
-        rightDown = False
 
       if event.key == pygame.K_SPACE:
         if gameStarted == False:
